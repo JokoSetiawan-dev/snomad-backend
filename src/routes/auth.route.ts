07267f6a5468623497779c5c27ec35a63b4ff017
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-12 22:10:26 +0700

    feat: create login and logout route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -4,5 +4,7 @@ import authController from '../controllers/auth.controller';
 const authRoutes = express.Router();
 
 authRoutes.post("/register", authController.register)
+authRoutes.post("/login", authController.login)
+authRoutes.post("/logout", authController.logout)
 
 export default authRoutes
