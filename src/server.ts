3cc7b53b4b3cee16f272c04cb7f2a5e2dc2b934e
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-18 22:35:02 +0700

    feat: initialize websocket server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
+import { Server } from 'socket.io';
 import app from './app';
+import http from 'http';
 import connectDB from './config/db';
+import socketServer from './services/websocketServer';
 
 const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+const io = new Server(server);
 
+// Initialize the WebSocket server
+socketServer(io);
 // Connect to database
 connectDB();
 
