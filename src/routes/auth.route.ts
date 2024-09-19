9e24e2037f0462a33132b2263db67f4c4caa30bf
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-19 21:16:52 +0700

    feat: implement rbac

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import authController from '../controllers/auth.controller';
+import { authenticationMiddleware } from '../middlewares/auth.middleware';
 
 const authRoutes = express.Router();
 
 authRoutes.post("/register", authController.register)
 authRoutes.post("/login", authController.login)
-authRoutes.post("/logout", authController.logout)
+authRoutes.post("/logout", authenticationMiddleware, authController.logout)
 
 export default authRoutes
--- src/routes/location.route.ts
+++ src/routes/location.route.ts
@@ -1,9 +1,22 @@
 import express from 'express';
 import locationController from '../controllers/location.controller';
+import { authenticationMiddleware, authorizationMiddleware } from '../middlewares/auth.middleware';
 
 const locationRoutes = express.Router();
 
-locationRoutes.post("/activate", locationController.shareLocationOn)
-locationRoutes.post("/deactivate", locationController.shareLocationOff)
+// Protected routes (seller needs to be authenticated to activate/deactivate location sharing)
+locationRoutes.post(
+    "/activate", 
+    authenticationMiddleware, // Ensure the user is authenticated
+    authorizationMiddleware({ role: ['seller'] }), // Only sellers can share their location
+    locationController.shareLocationOn // Activate location sharing
+);
 
-export default locationRoutes
+locationRoutes.post(
+    "/deactivate", 
+    authenticationMiddleware, // Ensure the user is authenticated
+    authorizationMiddleware({ role: ['seller'] }), // Only sellers can stop sharing their location
+    locationController.shareLocationOff // Deactivate location sharing
+);
+
+export default locationRoutes;
