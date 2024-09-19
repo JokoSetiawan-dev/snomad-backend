d9d255e8c189340a3fdf113685779808fd26defd
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-20 06:51:48 +0700

    feat: implement cors for security

---
+++ src/middlewares/cors.middleware.ts
@@ -0,0 +1,15 @@
+import cors from 'cors';
+
+// Configure allowed origins
+const allowedOrigins = ['*'];
+
+export const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allows cookies to be sent with requests
+};
--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,16 @@ import app from './app';
 import http from 'http';
 import connectDB from './config/db';
 import socketServer from './services/websocketServer';
+import helmet from 'helmet'
+import cors from 'cors';
+import { corsOptions } from './middlewares/cors.middleware';
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(helmet());
+app.use(cors(corsOptions));
 
 // Initialize the WebSocket server
 socketServer(io);
