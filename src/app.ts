3b7ab9207f717e89d9222955a5b46757dc452234
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-20 22:38:01 +0700

    feat: implement api limitter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route"
 import passwordRoutes from './routes/password.route';
+import { apiLimiter } from './middlewares/rateLimiter.middleware';
 
 const routes = express.Router();
 const app = express();
@@ -12,7 +13,7 @@ app.use(cookieParser()); // Parse cookies
 
 app.get('/', (req, res) => res.send('API is running'));
 
-app.use("/auth", authRoutes)
-app.use("/auth/password", passwordRoutes)
+app.use("/auth", apiLimiter,authRoutes)
+app.use("/auth/password", apiLimiter,passwordRoutes)
 
 export default app;
