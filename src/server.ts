40db5cb5cc6860f44b4a2743fa489d3aaf0c0fec
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-09-20 22:38:27 +0700

    feat: implement morgan for security

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import socketServer from './services/websocketServer';
 import helmet from 'helmet'
 import cors from 'cors';
 import { corsOptions } from './middlewares/cors.middleware';
+import { logger } from './middlewares/logger.middleware';
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
@@ -13,6 +14,9 @@ const io = new Server(server);
 
 app.use(helmet());
 app.use(cors(corsOptions));
+
+// Use morgan middleware
+app.use(logger(process.env.NODE_ENV as string));
 
 // Initialize the WebSocket server
 socketServer(io);
