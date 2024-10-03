601fd8668fe181cd5dbff95db56d08b220290506
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-10-03 17:24:13 +0700

    feat: cadd store and store menu routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route"
 import passwordRoutes from './routes/password.route';
 import { apiLimiter } from './middlewares/rateLimiter.middleware';
+import locationRoutes from './routes/location.route';
+import storeRoutes from './routes/store.route';
+import storeMenuRoutes from './routes/storeMenu.route';
 
 const routes = express.Router();
 const app = express();
@@ -15,5 +18,7 @@ app.get('/', (req, res) => res.send('API is running'));
 
 app.use("/auth", apiLimiter,authRoutes)
 app.use("/auth/password", apiLimiter,passwordRoutes)
+app.use("/location", apiLimiter,locationRoutes)
+app.use("/store", apiLimiter,storeRoutes, storeMenuRoutes)
 
 export default app;
