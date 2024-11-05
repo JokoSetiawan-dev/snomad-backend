676086570551f09d48c291718c960c5d34aa3001
JokoSetiawan-dev <jokosetiawan.career@gmail.com>
2024-11-05 13:24:51 +0700

    feat: google auth

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,16 @@ import storeRoutes from './routes/store.route';
 import storeMenuRoutes from './routes/storeMenu.route';
 import reviewRoutes from './routes/review.route';
 import favouriteStoreRoutes from './routes/favouriteStore.route';
+import notificationRoutes from './routes/notification.route';
+import trendsRoutes from './routes/trends.route';
+import dotenv from 'dotenv';
+import googleAuthController from './controllers/googleAuth.controller';
+import passport from 'passport';
+import session, { SessionOptions } from 'express-session';
+import googleAuthRoutes from './routes/googleAuth.route';
+
+dotenv.config();
+
 
 const routes = express.Router();
 const app = express();
@@ -16,12 +26,32 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // Parse cookies
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+      maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+  } as session.SessionOptions));
+  
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  // Initialize Google Strategy
+  googleAuthController.initializeGoogleStrategy();
+
 app.get('/', (req, res) => res.send('API is running'));
 
-app.use("/auth", apiLimiter,authRoutes)
+app.use("/auth",authRoutes, googleAuthRoutes)
 app.use("/auth/password", apiLimiter,passwordRoutes)
 app.use("/location", apiLimiter,locationRoutes)
 app.use("/store", apiLimiter,storeRoutes, storeMenuRoutes, reviewRoutes)
 app.use("/favourites", apiLimiter,favouriteStoreRoutes)
+app.use("/favourites", apiLimiter,favouriteStoreRoutes)
+app.use("/notifications", apiLimiter,notificationRoutes)
+app.use("/notifications", apiLimiter,notificationRoutes)
+app.use("/trends", apiLimiter,trendsRoutes)
 
 export default app;
